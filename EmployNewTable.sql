017c60f506422e61313f944d30681c7f68d543e3
Prasanna <prassucp@gmail.com>
2020-01-02 17:11:45 -0800

    ch

---
+++ EmployNewTable.sql
@@ -0,0 +1,22 @@
+drop table if exists EmployNew;
+
+Create Table EmployNew
+(
+    Empno int AUTO_INCREMENT,
+    Name varchar(50), 
+    Dept varchar(30),
+    Desig varchar(30),
+    Basic INT,
+    primary key(Empno)
+)
+;
+
+
+Insert into EmployNew(Name,Dept,Desig,Basic) values
+('kiran kumar kailasam','java','developer',42233),
+('Vishnu Vardhan Reddy','dotnet','programmer',42133),
+('Ram Kishan','java','developer',82233),
+('Kareem','dotnet','programmer',52234),
+('Sheik Sulthan Mohammad','dotnet','developer',42555),
+('suhaas','java','programmer',65523),
+('karthik','aws','deployment',67444)
