33d7da065b79da366eac183f780834c2e3c2b9f7
Prasanna <prassucp@gmail.com>
2020-01-02 17:14:45 -0800

    ch

---
+++ Exercise_Jan2.sql
@@ -0,0 +1,14 @@
+--> Display all records from EmployNew Table
+whose name starts and ends with same char. 
+
+--> Display all records from Employnew Table
+who have 2 part name 
+
+--> Display all records from EmployNew Table
+Who have 3 part name 
+
+-->  Display all records from EmployNew Table
+Whose 2 parts name starts with same char. 
+
+-->  Display all records from EmployNew Table
+Whose 3 parts name starts with same char. 
